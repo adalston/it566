848499150bf1cf41a3c416c34b9ecfce8d935c1d
Anthony Alston <75923000+adalston@users.noreply.github.com>
2022-09-30 01:33:41 -0400

    Initial Commit

---
+++ python/home_inventory/src/home_inventory.py
@@ -0,0 +1,81 @@
+"""Implements Home Inventory data structures and operations."""
+
+from asyncio.windows_events import NULL
+from contextlib import nullcontext
+from pathlib import Path
+import sys
+import string
+import json
+import os
+
+class HomeInventory():
+    """Implements Home Inventory data structures and operations."""
+
+    def __init__(self):
+        """Initialize Home Inventory object."""
+        self.dictionary = None
+        self.inventory_file = NULL
+
+    def new_inventory(self):
+        """Initialize new dictionary to store inventory data."""
+        if self.dictionary != None:
+            user_input = input("Save current inventory? (y/n): ")
+            match user_input.lower():
+                case 'y':
+                    self.save_inventory()
+                    self._initialize_home_inventory_dictionary()
+                case 'n':
+                    self._initialize_home_inventory_dictionary()
+                case _:
+                    self._initialize_home_inventory_dictionary()
+        else:
+            self._initialize_home_inventory_dictionary()
+
+    def load_inventory(self):
+        """Load inventory from file."""
+        self._list_inventory_files()
+        file_path = self._get_file_path()
+        with open(file_path, 'r', encoding='UTF-8') as self.inventory_file:
+            self.dictionary = json.load(self.inventory_file)
+        
+    def save_inventory(self):
+        """Save inventory to file."""
+        if self.dictionary != None:
+            file_path = self._get_file_path()
+            with open(file_path, 'w', encoding='UTF-8') as self.inventory_file:
+                self.inventory_file.write(json.dumps(self.dictionary))
+
+    def add_item(self, item_name, item_count):
+        assert self.dictionary != None
+        self.dictionary[item_name] = item_count
+
+    def list_inventory(self):
+        """List inventory to console."""
+        print('----- Current Inventory ----')
+        for key, value in self.dictionary.items():
+            print(key, " : ", value)
+
+    def _get_file_path(self):
+        """Get flle path from user."""
+        f_path = input("Please enter path and filename: ")
+        return f_path
+
+    def _close_files(self):
+        """Close open inventory file"""
+        self.inventory_file.close()
+
+    def _list_inventory_files(self):
+        """List all files in directory with _inventory.json file suffix."""
+        print('JSON Files found in current directory')
+        for (root, dirs, file) in os.walk(Path.cwd()):
+            for f in file:
+                if '.json' in f:
+                    print(f)
+       
+    def _initialize_home_inventory_dictionary(self):
+        print("Initializing new Home Inventory...")
+        self.dictionary = {}
+        print("New Home Inventory Initialized")
+
+
+
